404ad513d48247af2937736451d2519790dd2c35
Waldemar Schik <Waldemar.Schik@geutebrueck.com>
2022-01-06 14:05:26 +0100

    feat(ng-sonar-ui): Add linting

--- apps/ng-sonar-test/src/app/app.module.ts
+++ apps/ng-sonar-test/src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
